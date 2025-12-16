1c1854f78d80186b075b7e50a87dc2d401718cbb
Soyebuzaman Naim <soyebuzamannaim@gmail.com>
2025-12-17 00:19:25 +0600

    user service done

--- src/main/java/bd/edu/seu/lms/repository/UserRepo.java
+++ src/main/java/bd/edu/seu/lms/repository/UserRepo.java
@@ -3,11 +3,10 @@ package bd.edu.seu.lms.repository;
 import bd.edu.seu.lms.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
-    boolean existsByEmail(String email);
     Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
